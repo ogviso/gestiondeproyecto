e50ab503015b45713961a941999a990687cdadc5
Julián Manesi <44536616+Julimanesi@users.noreply.github.com>
2020-06-21 12:27:32 -0300

    Update EscalaHorasOB.cs

--- WebApplication2/Models/EscalaHorasOB.cs
+++ WebApplication2/Models/EscalaHorasOB.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,5 +9,11 @@ namespace WebApplication2.Models
     {
         public int Id { get; set; }
         public float PorcentajeAumento { get; set; }
+
+        public EscalaHorasOB(int id, float porcentaje)
+        {
+            this.Id = id;
+            this.PorcentajeAumento = porcentaje;
+        }
     }
 }
