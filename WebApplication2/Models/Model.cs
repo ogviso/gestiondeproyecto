82d374c5d4bacc891755d16052a800552601bda6
Julián Manesi <44536616+Julimanesi@users.noreply.github.com>
2020-06-21 12:29:15 -0300

    Update Model.cs

--- WebApplication2/Models/Model.cs
+++ WebApplication2/Models/Model.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -28,6 +28,59 @@ namespace WebApplication2.Models
         public DbSet<Proyecto> Proyecto { get; set; }
         public DbSet<Tarea> Tarea { get; set; }
 
+        //--------------------------------Agregado por Julian Manesi---------------------------------------
+        public void AgregarClientes(Cliente cliente) { Cliente.Add(cliente); }
+        public void AgregarEmpleados(Empleado empleado) { Empleado.Add(empleado); }
+        public void AgregarProyectos(Proyecto proyecto) { Proyecto.Add(proyecto); }
+        public Proyecto GetProyecto(int idProyecto)
+        {
+            Proyecto p = null;
+            foreach (Proyecto proyecto in Proyecto)
+            {
+                if (proyecto.Id == idProyecto)
+                {
+                    p = proyecto;
+                }
+            }
+            return p;
+        }
+        public Empleado GetEmpleado(int idEmpledo)
+        {
+            Empleado e = null;
+            foreach (Empleado empleado in Empleado)
+            {
+                if (empleado.Id == idEmpledo)
+                {
+                    e = empleado;
+                }
+            }
+            return e;
+        }
+        public Cliente GetCliente(int idCliente)
+        {
+            Cliente c = null;
+            foreach (Cliente cliente in Cliente)
+            {
+                if (cliente.Id == idCliente)
+                {
+                    c = cliente;
+                }
+            }
+            return c;
+        }
+        public List<Proyecto> ObtenerProyectosDeEmpleado(int idEmpleado)
+        {
+            List<Proyecto> proyectos = new List<Proyecto>();
+            foreach (Proyecto proyecto in Proyecto)
+            {
+                foreach (Tarea tarea in proyecto.ObtenerTareas())
+                {
+                    if (tarea.IdEmpleado == idEmpleado && !proyectos.Contains(proyecto))
+                        proyectos.Add(proyecto);
+                }
+            }
+            return proyectos;
+        }
     }
 
 
