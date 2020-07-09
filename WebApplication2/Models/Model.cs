829a6a34d3bfbbdc4a0d7872a9a46524905f3ec5
julimanesi94 <julimanesi94@gmail.com>
2020-07-09 16:23:56 -0300

    Cambios Model

--- WebApplication2/Models/Model.cs
+++ WebApplication2/Models/Model.cs
@@ -29,9 +29,22 @@ namespace WebApplication2.Models
         public DbSet<Tarea> Tarea { get; set; }
 
         //--------------------------------Agregado por Julian Manesi---------------------------------------
-        public void AgregarClientes(Cliente cliente) { Cliente.Add(cliente); }
-        public void AgregarEmpleados(Empleado empleado) { Empleado.Add(empleado); }
-        public void AgregarProyectos(Proyecto proyecto) { Proyecto.Add(proyecto); }
+       public void AgregarClientes(int id, string nombre) { Cliente.Add(new Cliente(id,nombre)); }
+        public void AgregarEmpleados(int id,string nombre, DateTime FechaIng) { Empleado.Add(new Empleado(id, nombre,FechaIng)); }
+        public void AgregarProyectos(int idCliente,int idProyecto,string nombre) { Proyecto.Add(new Proyecto(idCliente,idProyecto,nombre)); }
+        public void AgregarPerfil(int id,Perfil.Tipo tipo,float valor) { Perfil.Add(new Perfil(id,tipo, valor)); }
+        public void AgregarEscalaAumxAntiguedad(int id,int limite,float porcentaje) { EscalaAumentoxAntiguedad.Add(new EscalaAumentoxAntiguedad(id, limite, porcentaje)); }
+        public void AgregarEscalaAumxhora(int id, int limite, float porcentaje) { EscalaAumentoxHora.Add(new EscalaAumentoxHora(id, limite, porcentaje)); }
+        public void AgregarEscalaAumxPerfil(int id, int limite, float porcentaje) { EscalaAumentoxPerfil.Add(new EscalaAumentoxPerfil(id, limite, porcentaje)); }
+
+        public void BorrarCliente( int idCliente) { Cliente.Remove(GetCliente(idCliente)); }  
+        public void BorrarEmpleado(int idEmpleado) { Empleado.Remove(GetEmpleado(idEmpleado));}
+        public void BorrarProyecto(int idProyecto) { Proyecto.Remove(GetProyecto(idProyecto));}
+
+        public void ModificarCliente(Cliente cliente) { Cliente.Update(cliente); }
+        public void ModificarEmpleado(Empleado empleado) { Empleado.Update(empleado); }
+        public void ModificarProyecto(Proyecto proyecto) { Proyecto.Update(proyecto); }
+
         public Proyecto GetProyecto(int idProyecto)
         {
             Proyecto p = null;
